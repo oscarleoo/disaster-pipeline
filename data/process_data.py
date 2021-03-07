32b404e8bbe3569c9f2099e7d14422cc9aaeeea5
Oscar Johansson <oscar@aiwizo.com>
2021-03-07 07:57:51 +0100

    added docstrings to process_data, and removed related == 2

--- data/process_data.py
+++ data/process_data.py
@@ -4,12 +4,33 @@ from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
+    """
+    Loading and compining the messages and categories csv files
+    --
+    Inputs:
+        messages_filepath: csv file contains messages
+        categories_filepath: csv file contains categories
+    Outputs:
+        df: the combined dataframe
+    """
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
     return messages.merge(categories, on='id', how='outer')
 
 
 def clean_data(df):
+    """
+    Preprocess the combined data in preperation for training
+    This includes making creating binary targets of the categories,
+    and removing duplicates
+    
+    I'm also removing the instances where related is 2 because I couldn't find an explanation of what that means.
+    --
+    Inputs:
+        df: messages and categories combined dataframe dataframe
+    Outputs:
+        df: cleaned dataframe
+    """ 
     categories = df.categories.str.split(';', expand=True)
     category_names = [c[:-2] for c in categories.iloc[0]]
     categories.columns = category_names
@@ -17,13 +38,21 @@ def clean_data(df):
     for column in categories:
         categories[column] = categories[column].str[-1]
         categories[column] = categories[column].astype(int)
-
+    categories = categories[categories.related != 2]
+        
     df = df.drop('categories', axis=1)
     df = pd.concat([df, categories], axis=1)
-    return df.drop_duplicates()
+    return df.dropna().drop_duplicates()
     
 
 def save_data(df, database_filename):
+    """
+    Saves the dataframe into a sqlite database 
+    --
+    Inputs:
+        df: messages and categories combined dataframe dataframe
+        database_filename: Name of the database file
+    """    
     engine = create_engine('sqlite:///{}'.format(database_filename))
     df.to_sql('disaster_data', engine, index=False)  
 
